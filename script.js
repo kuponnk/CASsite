3d6ad0e81abd13347ecc5787d2d55dfdf62e8ce1
kuponnk <152103456+kuponnk@users.noreply.github.com>
2024-05-31 21:38:59 +0200

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 
 const videoFrame = document.getElementById('videoFrame');
-const videos = ['https://www.youtube.com/embed/DhKJai6I8p4', 'https://www.youtube.com/embed/ZSzaEqAyAEc'];
+const videos = ['https://www.youtube.com/embed/QJ5anheRbEM', 'https://www.youtube.com/embed/DhKJai6I8p4', 'https://www.youtube.com/embed/ZSzaEqAyAEc', 'https://www.youtube.com/embed/nzrWOEdbKW0'];
 
 let currentVideoIndex = 0;
 
